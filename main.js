4b94c6467580971e66434e58206b5bc618411dde
yasmen maged <gasmenmaged@gmail.com>
2024-03-13 09:45:06 +0200

    Add Update Color Feature

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 let width = 100;
 let height = 100;
+const container =  document.getElementById('container');
 
 let updateDimensions = setInterval(function () {
   width+=15;
   height+=5;
-  document.getElementById('container').style.width = width + "px";
-  document.getElementById('container').style.height = height + "px";
+  container.style.width = width + "px";
+  container.style.height = height + "px";
 },1000);
+
+function getRandomColor(){
+  let hexaLetters = "0123456789ABCDEF";
+  let color = "#";
+  for(let i = 0; i < 6; i++){
+    color += hexaLetters[Math.floor(Math.random()*16)];
+  }
+  return color;
+}
+
+function updateColor(){
+  container.style.background = getRandomColor();
+}
+
+setInterval(updateColor,1000);
