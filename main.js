67459c8f176953d02e032b0d22973d389940de67
yasmen maged <gasmenmaged@gmail.com>
2024-03-13 09:46:43 +0200

    Finish Task

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 let width = 100;
 let height = 100;
-const container =  document.getElementById('container');
+const container = document.getElementById("container");
 
 let updateDimensions = setInterval(function () {
-  width+=15;
-  height+=5;
+  width += 15;
+  height += 5;
   container.style.width = width + "px";
   container.style.height = height + "px";
-},1000);
+}, 1000);
 
-function getRandomColor(){
+function getRandomColor() {
   let hexaLetters = "0123456789ABCDEF";
   let color = "#";
-  for(let i = 0; i < 6; i++){
-    color += hexaLetters[Math.floor(Math.random()*16)];
+  for (let i = 0; i < 6; i++) {
+    color += hexaLetters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-function updateColor(){
+function updateColor() {
   container.style.background = getRandomColor();
 }
 
-setInterval(updateColor,1000);
+setInterval(updateColor, 1000);
